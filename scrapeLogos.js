38ae6527ded3eb12ce729d4f0c687137bd8f6394
AJ Amelio <aj.amelio90@gmail.com>
2018-06-12 11:13:54 -0400

    Update scrapeLogos.js

--- scrapeLogos.js
+++ scrapeLogos.js
@@ -3,42 +3,65 @@ const cheerio = require("cheerio");
 const fs = require("fs");
 const request = require("request");
 const xlsx = require("xlsx");
-const {URL} = require("url");
+const { URL } = require("url");
 
-const workbook = xlsx.readFile('sites.xlsx');
+const workbook = xlsx.readFile('all_logos_test.xlsx');
 const sheet_name_list = workbook.SheetNames;
 const sites = xlsx.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]])
 
-let download = function(uri, filename, callback){
-  request.head(uri, function(err, res, body){
-    request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
-  });
+let download = function (uri, filename, callback) {
+    request.head(uri, function (err, res, body) {
+        request(uri).pipe(fs.createWriteStream(filename)).on('close', callback); 
+    });
 };
 
+let increment = 0;
+
 sites.forEach((site) => {
-  let url = new URL(site.site_url)
-  axios.get(url.origin).then((response) => {
-    let html = response.data;
-    let $ = cheerio.load(html)
-    $("img").each(function(index) {
-      let img = $(this).attr("src");
-      if (img.indexOf("logo") >= 0) {
-        let imgLink;
-        if (img.startsWith("http") || img.startsWith("www")) {
-          imgLink = img
-        } else {
-          imgLink = url.origin + img
+    let cleanURL;
+    if (site.site_url.startsWith("http")) {
+        cleanURL = site.site_url;
+    } else {
+        cleanURL = "http://www." + site.site_url;
+    }
+    let url = new URL(cleanURL)
+    axios.get(url.origin).then((response) => {
+        if (response.status === 200 && response !== undefined) {
+            let html = response.data;
+            let $ = cheerio.load(html)   
+            $("img").each(function (index) {               
+                if (index < 4) {
+                    let img = $(this).attr("src");
+                    if (img.indexOf("logo") >= 0) {
+                        let imgLink;
+                        if (img.indexOf("http") >= 0 || img.indexOf("www") >= 0) {
+                            imgLink = img
+                        } else {
+                            imgLink = url.origin + "/" + img
+                        }
+                        let obj = {
+                            siteName: site.site_name.replace(/\s/g, '').replace(/[^\w\s]/gi, ''),
+                            logo: imgLink.split("?").shift(),
+                            host: url.origin,
+                            imagepath: img,
+                            siteId: site.site_id,
+                            extension: "." + imgLink.slice((imgLink.lastIndexOf(".") - 1 >>> 0) + 2).split("?").shift().split(";").shift()
+                        }              
+                        axios.get(obj.logo).then((response) => {
+                            increment++;
+                            if (response.status === 200) {
+                                if (obj.extension == ".png" || obj.extension == ".jpg" || obj.extension == ".jpeg" || obj.extension === ".svg") {
+                                    console.log(obj)
+                                    download(obj.logo, "output/" + obj.siteName + "-" + obj.siteId + "-" + increment + obj.extension, function () { });
+                                }
+                            }
+                        }).catch((e) => {
+                        })
+                    }
+                }
+            });           
         }
-        let obj = {
-          siteName: site.site_name,
-          logo: imgLink,
-          siteId: site.site_id,
-          extension: "." + imgLink.slice((imgLink.lastIndexOf(".") - 1 >>> 0) + 2)
-        }
-        download(obj.logo, "output/" + obj.siteName + "-" + obj.siteId + obj.extension, function(){
-        });
-      }
+    }).catch((e) => {
     });
-  }).catch((e) => {
-  });
 })
+
